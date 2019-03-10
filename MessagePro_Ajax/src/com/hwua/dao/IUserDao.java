9a7c4524a1e533e2c40bb62c04b458bd61be7e26
zgf <guangfeizeng163@163.com>
2019-03-11 00:28:37 +0800

    第二次更新

--- MessagePro_Ajax/src/com/hwua/dao/IUserDao.java
+++ MessagePro_Ajax/src/com/hwua/dao/IUserDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.hwua.entity.User;
 
 public interface IUserDao {
-    public User query(User user) throws SQLException;//鏌ヨ鐢ㄦ埛
+    public User query(User user) throws SQLException;//查询用户
     public int save(User user) throws SQLException;//鎻掑叆鐢ㄦ埛
     public List<User> query() throws SQLException;//鏌ヨ鎵�鏈夌殑鐢ㄦ埛淇℃伅
     public User query(int sendid) throws SQLException;
